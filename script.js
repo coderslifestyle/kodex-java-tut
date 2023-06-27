9d7ca727843cecab93a39d3fadc69bacf12ce085
Ebuka Caius <114081577+coderslifestyle@users.noreply.github.com>
2023-06-27 13:21:14 +0000

    Update Script.js

--- script.js
+++ script.js
@@ -380,3 +380,7 @@ const inputYear =  '1991';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 10);
 
+console.log(Number('Donald'));
+console.log(typeof NaN);
+console.log(String(23), 23);
+
